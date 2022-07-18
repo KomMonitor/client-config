66f0af701af5fd1da85ef0f42ebcf60f9b4dc961
cDanowski <christian.danowski-buhren@hs-bochum.de>
2022-07-18 10:03:16 +0200

    upgrade to openAPI v3 router and YAML resource

--- index.js
+++ index.js
@@ -6,11 +6,36 @@ var fs = require('fs'),
     path = require('path'),
     http = require('http');
 
-var swaggerTools = require('swagger-tools');
+var oas3Tools = require('oas3-tools');
 var jsyaml = require('js-yaml');
 var cors = require('cors');
 var express = require('express');
 var serverPort = process.env.PORT;
+
+// const session = require('express-session');
+// const Keycloak = require('keycloak-connect');
+
+// const memoryStore = new session.MemoryStore();
+// const kcConfig = {
+//   clientId: 'kommonitor-client-config',
+//   bearerOnly: true,
+//   serverUrl: 'http://localhost:8090',
+//   realm: 'kommonitor-gib',
+//   realmPublicKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjaWRPI4mgkwzii8riv4G0Gq4Znjq/UY3Lq3b0+DUUnCqs9ytZ5+1ONGDHi54yV8DjlVmlkay6ckP9QPOgdP3qRa/5y1pG7RqKdnK8twFZ6saqAbhE12gZcXRK7aWD/qQ/VVxOsIYSfmeGY+zzNORN9PGs7lSwZSR9XsFzyolpl4do/WDU/QsJry8nHwOklJjLPlI3+U7gpXKgYtzXtTV8ex93gtrmeWpwNlhGwZ9CSaO/mEKxJpXGtDFmiNGuW8MoluoyOxu0Qb4tkQfmfc4V9polHNDhnprJGZhoSdsBYB49AJ2E1bTVAjQ+2ENfX9K77dtnU0Z7p7E/iYyVaVtlQIDAQAB'
+// };
+
+// const keycloak = new Keycloak({ store: memoryStore }, kcConfig);
+
+// swaggerRouter configuration
+var options = {
+  routing: {
+      controllers: path.join(__dirname, './controllers')
+  },
+};
+
+var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+
+var openApiApp = expressAppConfig.getApp();
 
 var app = express();
 
@@ -19,38 +44,16 @@ const corsOptions = {
   exposedHeaders: ['Access-Control-Allow-Origin','Location','Connection','Content-Type','Date','Transfer-Encoding','Origin','X-Requested-With', 'Accept'],
   origin: "*"
 };  
-app.use(cors(corsOptions));
 
-// swaggerRouter configuration
-var options = {
-  swaggerUi: path.join(__dirname, '/swagger.json'),
-  controllers: path.join(__dirname, './controllers'),
-  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
-};
+// Add headers
+app.use(/.*/, cors(corsOptions));
 
-// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
+for (let i = 2; i < openApiApp._router.stack.length; i++) {
+  app._router.stack.push(openApiApp._router.stack[i]);
+}
 
-// Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
-
-  // Validate Swagger requests
-  app.use(middleware.swaggerValidator());
-
-  // Route validated requests to appropriate controller
-  app.use(middleware.swaggerRouter(options));
-
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());
-
-  // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-  });
-
+// Start the server
+http.createServer(app).listen(serverPort, function () {
+  console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+  console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
